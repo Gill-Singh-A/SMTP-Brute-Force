30a41f14c340f32bec910f70819ccac0daa20681
Gill-Singh-A <amansg22@iitk.ac.in>
2024-04-16 23:17:50 +0530

    Added Command Line Arguments

--- main.py
+++ main.py
@@ -22,5 +22,58 @@ def get_arguments(*args):
         parser.add_option(arg[0], arg[1], dest=arg[2], help=arg[3])
     return parser.parse_args()[0]
 
+port = 25
+
 if __name__ == "__main__":
-    pass
+    arguments = get_arguments(('-s', "--server", "server", "Target SMTP Server"),
+                              ('-p', "--port", "port", f"Port of Target SMTP Server (Default={port})"),
+                              ('-u', "--users", "users", "Target Users (seperated by ',') or File containing List of Users"),
+                              ('-p', "--password", "password", "Passwords (seperated by ',') or File containing List of Passwords"),
+                              ('-c', "--credentials", "credentials", "Name of File containing Credentials in format ({user}:{password})"))
+    if not arguments.server:
+        display('-', f"Please specify {Back.YELLOW}Target Server{Back.RESET}")
+        exit(0)
+    if not arguments.port:
+        arguments.port = port
+    else:
+        arguments.port = int(arguments.port)
+    if not arguments.credentials:
+        if not arguments.users:
+            display('-', f"Please specify {Back.YELLOW}Target Users{Back.RESET}")
+            exit(0)
+        else:
+            try:
+                with open(arguments.users, 'r') as file:
+                    arguments.users = [user for user in file.read().split('\n') if user != '']
+            except FileNotFoundError:
+                arguments.user = arguments.user.split(',')
+            except:
+                display('-', f"Error while Reading File {Back.YELLOW}{arguments.users}{Back.RESET}")
+                exit(0)
+            display(':', f"Users Loaded = {Back.MAGENTA}{len(arguments.user)}{Back.RESET}")
+        if not arguments.password:
+            display('-', f"Please specify {Back.YELLOW}Passwords{Back.RESET}")
+            exit(0)
+        else:
+            try:
+                with open(arguments.password, 'r') as file:
+                    arguments.password = [password for password in file.read().split('\n') if password != '']
+            except FileNotFoundError:
+                arguments.password = arguments.password.split(',')
+            except:
+                display('-', f"Error while Reading File {Back.YELLOW}{arguments.password}{Back.RESET}")
+                exit(0)
+            display(':', f"Passwords Loaded = {Back.MAGENTA}{len(arguments.password)}{Back.RESET}")
+        arguments.credentials = []
+        for user in arguments.user:
+            for password in arguments.password:
+                arguments.credentials.append([user, password])
+    else:
+        try:
+            with open(arguments.credentials, 'r') as file:
+                arguments.credentials = [[credential.split(':')[0], ':'.join(credential.split(':')[1:])] for credential in file.read().split('\n') if len(credential.split(':')) > 1]
+        except:
+            display('-', f"Error while Reading File {Back.YELLOW}{arguments.credentials}{Back.RESET}")
+            exit(0)
+    total_credentials = len(arguments.credentials)
+    display('+', f"Total Credentials = {Back.MAGENTA}{total_credentials}{Back.RESET}")
