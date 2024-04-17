d6175288a7c062d4c2f170fa91549f95d7eea21c
Gill-Singh-A <amansg22@iitk.ac.in>
2024-04-17 22:32:03 +0530

    Fixed Brute Force Loop Condition

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ def login(mail_server, port, user, password):
 def brute_force(thread_index, mail_server, port, credentials):
     successful_logins = {}
     for credential in credentials:
-        status = ''
-        while status != True and status != False:
+        status = ['']
+        while status[0] != True and status[0] != False:
             status = login(mail_server, port, credential[0], credential[1])
             if status[0] == True:
                 successful_logins[credential[0]] = credential[1]
