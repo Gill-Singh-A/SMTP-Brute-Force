2155aef7be2cdd8065608a5468940af52434e5c9
Gill-Singh-A <amansg22@iitk.ac.in>
2024-06-12 03:44:54 +0530

    Added Ignore Errors Option

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ def get_arguments(*args):
     return parser.parse_args()[0]
 
 port = 25
+ignore_errors = True
 lock = Lock()
 
 def login(mail_server, port, user, password):
@@ -58,6 +59,8 @@ def brute_force(thread_index, mail_server, port, credentials):
             else:
                 with lock:
                     display(' ', f"Thread {thread_index+1}:{status[1]:.2f}s -> {Fore.CYAN}{credential[0]}{Fore.RESET}:{Fore.GREEN}{credential[1]}{Fore.RESET} => {Fore.YELLOW}Error Occured : {Back.RED}{status[0]}{Fore.RESET}{Back.RESET}")
+                if ignore_errors:
+                    break
     return successful_logins
 def main(server, port, credentials):
     successful_logins = {}
@@ -83,6 +86,7 @@ if __name__ == "__main__":
                               ('-u', "--users", "users", "Target Users (seperated by ',') or File containing List of Users"),
                               ('-P', "--password", "password", "Passwords (seperated by ',') or File containing List of Passwords"),
                               ('-c', "--credentials", "credentials", "Name of File containing Credentials in format ({user}:{password})"),
+                              ('-i', "--ignore-errors", "ignore_errors", f"Ignore Errors (True/False, Default={ignore_errors})"),
                               ('-w', "--write", "write", "CSV File to Dump Successful Logins (default=current data and time)"))
     if not arguments.server:
         display('-', f"Please specify {Back.YELLOW}Target Server{Back.RESET}")
@@ -129,6 +133,8 @@ if __name__ == "__main__":
         except:
             display('-', f"Error while Reading File {Back.YELLOW}{arguments.credentials}{Back.RESET}")
             exit(0)
+    if arguments.ignore_errors == False:
+        ignore_errors = False
     if not arguments.write:
         arguments.write = f"{date.today()} {strftime('%H_%M_%S', localtime())}.csv"
     display('+', f"Total Credentials = {Back.MAGENTA}{len(arguments.credentials)}{Back.RESET}")
